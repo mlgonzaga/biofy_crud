d44daab13425b4d5217423ab26509a72b7f0937f
Mateus Luiz Gonzaga <mateusluizgonzaga@gmail.com>
2024-12-08 15:04:26 -0300

    ajustando docker-compose

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,22 @@ const routes = require('./routes/routes')
 require('dotenv').config()
 const app = express()
 
-app.use(cors({
-    origin: 'http://localhost:5174', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }));
+
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
 
 
 const PORT = process.env.PORT || 3002
@@ -22,7 +33,7 @@ app.use('/', routes)
 app.use('/items', routes)
 
 
-app.listen(PORT, ()=>{
+app.listen(PORT, '0.0.0.0',()=>{
     console.log(`Server running on port ${PORT}!`)
 })
 
